97f114119af565b4b1b0520f25fbf5f5d5d369e3
rohantor <rohan.magar@torinit.ca>
2023-01-31 15:36:43 +0530

    added prefilled input field using query param

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,6 +1,7 @@
-import { Dispatch, SetStateAction, useState, useRef } from 'react'
+import { Dispatch, SetStateAction, useState, useRef, useMemo } from 'react'
 import { ProductInterface } from '../Interface'
 import { FormValidator } from '../utils/helper'
+import { useLocation } from 'react-router-dom'
 interface FormInterface {
   setCardDetailsArray: Dispatch<SetStateAction<ProductInterface[]>>
 }
@@ -14,8 +15,7 @@ export default function Form(props: FormInterface) {
     wishlisted: false,
   })
   const InputRef = useRef<HTMLInputElement>(null)
-  const ClearForm = ()=>{
-
+  const ClearForm = () => {
     setNewItem({
       title: '',
       url: '',
@@ -25,127 +25,132 @@ export default function Form(props: FormInterface) {
       wishlisted: false,
     })
   }
+  function useQuery() {
+    const { search } = useLocation()
+
+    return useMemo(() => new URLSearchParams(search), [search])
+  }
+  let query = useQuery()
+
   return (
     <>
-    
-        <div
-          className='OuterModal'
-          onClick={(event) => {
-            event.stopPropagation()
-          }}
-        >
-          <div>
-            <input type='text' disabled value={'React test'} ref={InputRef} />
-            <form
-              onSubmit={(event) => event.preventDefault()}
-              style={{ display: 'left', justifyContent: 'space-around' }}
-            >
-              <div className='formDiv'>
-                <label htmlFor='input'> Product Name</label>
-                <input
-                  placeholder=' product name'
-                  type='text'
-                  value={newItem.title}
-                  name='input'
-                  className='inputForm'
-                  onChange={(event) => {
-                    setNewItem((prv) => {
-                      return { ...prv, title: event.target.value }
-                    })
-                  }}
-                />
-              </div>
-              <div className='formDiv'>
-                <label htmlFor='input'> Product Id </label>
-                <input
-                  placeholder='add product id'
-                  type='number'
-                  name='input'
-                  value={newItem.id}
-                  className='inputForm'
-                  onChange={(event) => {
-                    setNewItem((prv) => {
-                      return {
-                        ...prv,
-                        id: parseInt(
-                          event.target.value === '' ? '0' : event.target.value
-                        ),
-                      }
-                    })
-                  }}
-                />
-              </div>{' '}
-              <div className='formDiv'>
-                <label htmlFor='input'> Product Description</label>
-                <input
-                  type='text'
-                  placeholder=' product description'
-                  name='input'
-                  value={newItem.description}
-                  onChange={(event) => {
-                    setNewItem((prv) => {
-                      return { ...prv, description: event.target.value }
-                    })
-                  }}
-                  className='inputForm'
-                />
-              </div>{' '}
-              <div className='formDiv'>
-                <label htmlFor='input'> Product Price</label>
-                <input
-                  type='number'
-                  name='input'
-                  value={newItem.price}
-                  onChange={(event) => {
-                    setNewItem((prv) => {
-                      return {
-                        ...prv,
-                        price: parseInt(
-                          event.target.value === '' ||
-                            event.target.value === 'NaN'
-                            ? '0'
-                            : event.target.value
-                        ),
-                      }
-                    })
-                  }}
-                  className='inputForm'
-                />
-              </div>
-              <div className='formDiv'>
-                <label htmlFor='input'> Product Url</label>
-                <input
-                  type='text'
-                  name='input'
-                  value={newItem.url}
-                  placeholder='Product Url'
-                  className='inputForm'
-                  onChange={(event) => {
-                    setNewItem((prv) => {
-                      return { ...prv, url: event.target.value }
-                    })
-                  }}
-                />
-              </div>
-              <br />
-              <div>
-                <button
-                  className='SubmitBtn'
-                  type='button'
-                  onClick={async () => {
-                    if (await FormValidator(newItem)) {
-                      console.log(InputRef?.current?.value)
-                      props.setCardDetailsArray((prv) => [...prv, newItem])
-
-                      ClearForm()
+      <div
+        className='OuterModal'
+        onClick={(event) => {
+          event.stopPropagation()
+        }}
+      >
+        <div>
+          <input type='text' disabled value={query.get('q')||'React test'} ref={InputRef} />
+          <form
+            onSubmit={(event) => event.preventDefault()}
+            style={{ display: 'left', justifyContent: 'space-around' }}
+          >
+            <div className='formDiv'>
+              <label htmlFor='input'> Product Name</label>
+              <input
+                placeholder=' product name'
+                type='text'
+                value={newItem.title}
+                name='input'
+                className='inputForm'
+                onChange={(event) => {
+                  setNewItem((prv) => {
+                    return { ...prv, title: event.target.value }
+                  })
+                }}
+              />
+            </div>
+            <div className='formDiv'>
+              <label htmlFor='input'> Product Id </label>
+              <input
+                placeholder='add product id'
+                type='number'
+                name='input'
+                value={newItem.id}
+                className='inputForm'
+                onChange={(event) => {
+                  setNewItem((prv) => {
+                    return {
+                      ...prv,
+                      id: parseInt(
+                        event.target.value === '' ? '0' : event.target.value
+                      ),
                     }
-                  }}
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-        
+                  })
+                }}
+              />
+            </div>{' '}
+            <div className='formDiv'>
+              <label htmlFor='input'> Product Description</label>
+              <input
+                type='text'
+                placeholder=' product description'
+                name='input'
+                value={newItem.description}
+                onChange={(event) => {
+                  setNewItem((prv) => {
+                    return { ...prv, description: event.target.value }
+                  })
+                }}
+                className='inputForm'
+              />
+            </div>{' '}
+            <div className='formDiv'>
+              <label htmlFor='input'> Product Price</label>
+              <input
+                type='number'
+                name='input'
+                value={newItem.price}
+                onChange={(event) => {
+                  setNewItem((prv) => {
+                    return {
+                      ...prv,
+                      price: parseInt(
+                        event.target.value === '' ||
+                          event.target.value === 'NaN'
+                          ? '0'
+                          : event.target.value
+                      ),
+                    }
+                  })
+                }}
+                className='inputForm'
+              />
+            </div>
+            <div className='formDiv'>
+              <label htmlFor='input'> Product Url</label>
+              <input
+                type='text'
+                name='input'
+                value={newItem.url}
+                placeholder='Product Url'
+                className='inputForm'
+                onChange={(event) => {
+                  setNewItem((prv) => {
+                    return { ...prv, url: event.target.value }
+                  })
+                }}
+              />
+            </div>
+            <br />
+            <div>
+              <button
+                className='SubmitBtn'
+                type='button'
+                onClick={async () => {
+                  if (await FormValidator(newItem)) {
+                    console.log(InputRef?.current?.value)
+                    props.setCardDetailsArray((prv) => [...prv, newItem])
+
+                    ClearForm()
+                  }
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>{' '}
     </>
