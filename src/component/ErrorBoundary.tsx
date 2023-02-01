1fb2f7acc92607f1562b0d4a1c1dae9df013c988
rohantor <rohan.magar@torinit.ca>
2023-02-01 11:04:54 +0530

    Error boundary

---
+++ src/component/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import {ErrorBoundary} from 'react-error-boundary'
+
+
+import React, { useState } from 'react'
+let arr="Error"
+function ErrorFallback({error, resetErrorBoundary}:{error:any,resetErrorBoundary:any}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+function ErrorComponent(){
+const [index,setIndex ] = useState(0)
+
+ return (
+ <>
+
+
+ {arr.map((i)=>{
+  return(<h1>{i}</h1>)
+ })}
+ </>
+ )
+}
+export default function ErrorBoundaryComponent() {
+  return (
+    <>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {
+       arr=["Solved"]
+      }}>
+        <ErrorComponent />
+      </ErrorBoundary>
+    </>
+  )
+}
+
+
