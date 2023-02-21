1fc2762fe20df87450e6349d1317bddf0d553716
rohantor <rohan.magar@torinit.ca>
2023-02-21 17:49:19 +0530

    console.log removed async, await can be used destructured the item

--- src/Pages/CartPage.tsx
+++ src/Pages/CartPage.tsx
@@ -4,42 +4,60 @@ import CardTemp from '../component/Card/ProductCard'
 import { CartProductInterface } from '../Interface'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootStateType } from '../store/rootReducer'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { CartActions } from '../store/CartReducer'
 import { nanoid } from 'nanoid'
+import { toast } from 'react-toastify'
 
 function CartPage() {
   const { cartItems } = useSelector((state: RootStateType) => state.cart)
-  console.log(cartItems)
   const dispatch = useDispatch()
-  const deleteHandler=(index:number)=>{
+  const deleteHandler = (index: number) => {
     dispatch(CartActions.RemoveCartItem(index))
-  
   }
   useEffect(() => {
-    axios
-      .get(process.env.REACT_APP_BASE_URL + 'cart')
-      .then((res) => res.data)
-      .then((data) => {
-        dispatch(CartActions.SetCart(data));
-      })
+    ;(async () => {
+      try {
+        const { data } = await axios.get(
+          process.env.REACT_APP_BASE_URL + 'cart'
+        )
+
+        dispatch(CartActions.SetCart(data))
+      } catch (err) {
+        let errorMessage = ''
+        if (err instanceof AxiosError) {
+          errorMessage = err.message
+        } else {
+          errorMessage = 'Unexpected error'
+        }
+        toast.error(errorMessage)
+      }
+    })()
   }, [])
   return (
     <GridTemplate>
-      
-      {cartItems?.map((item: CartProductInterface, index: number) => {
-        return (
-          <CardTemp
-          key={nanoid()}
-            deleteHandler={{ fn: deleteHandler, identifier: index,resource:'cart' }}
-            title={item.title}
-            image={item.image}
-            description={item.description}
-            id={item.id}
-            price={item.price}
-          />
-        )
-      })} 
+      {cartItems?.map(
+        (
+          { title, image, description, id, price }: CartProductInterface,
+          index: number
+        ) => {
+          return (
+            <CardTemp
+              key={nanoid()}
+              deleteHandler={{
+                fn: deleteHandler,
+                identifier: index,
+                resource: 'cart',
+              }}
+              title={title}
+              image={image}
+              description={description}
+              id={id}
+              price={price}
+            />
+          )
+        }
+      )}
     </GridTemplate>
   )
 }
