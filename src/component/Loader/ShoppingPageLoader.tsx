05870c0b0b1af062c908f1863e930cd5717d4c70
rohantor <rohan.magar@torinit.ca>
2023-02-22 12:33:10 +0530

    added async await

--- src/component/Loader/ShoppingPageLoader.tsx
+++ src/component/Loader/ShoppingPageLoader.tsx
@@ -3,29 +3,35 @@ import Loader from 'react-js-loader'
 import { CardActions } from '../../store/CardReducer'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootStateType } from '../../store/rootReducer'
+import axios, { AxiosError } from 'axios'
 export default function ShoppingPageLoader(Wrapper: React.FC<any>) {
   function Loading(props: any) {
     const { cardDetailsArray } = useSelector(
       (state: RootStateType) => state.card
     )
- 
+
     const [loading, setLoading] = useState({ text: 'Loading', status: false })
     const dispatch = useDispatch()
 
     useEffect(() => {
-      if(cardDetailsArray?.length === 0) {
+      if (cardDetailsArray?.length === 0) {
         setLoading({ text: 'Loading', status: true })
-
-        fetch(process.env.REACT_APP_BASE_URL + 'products/')
-          .then((res) => res.json())
-          .then((json) => {
-            dispatch(CardActions.SetCards(json))
+        ;(async () => {
+          try {
+            const { data } = await axios.get(
+              process.env.REACT_APP_BASE_URL + 'products/'
+            )
+            dispatch(CardActions.SetCards(data))
             setLoading({ text: 'Loading', status: false })
-          })
-          .catch(() => {
-            
-            setLoading({ text: 'Server Refused to connect', status: true })
-          })
+          } catch (error) {
+            if (error instanceof AxiosError) {
+              setLoading({
+                text: `Error :${error.message} Server Refused to connect`,
+                status: true,
+              })
+            }
+          }
+        })()
       }
     }, [])
 
